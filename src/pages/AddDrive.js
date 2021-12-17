4ae5571b2088a8d45b20d01a61605f9f5c6db18b
caoyufei <atcaoyufei@gmail.com>
2021-12-17 22:55:25 +0800

    0.12

--- src/pages/AddDrive.js
+++ src/pages/AddDrive.js
@@ -1,18 +1,35 @@
 import React from "react";
 
+const auth = (e) => {
+    const value = e.target.value;
+    console.log(value)
+}
+
 export const AddDrive = () => {
     return (
-        <div className="w-5/12 mx-auto">
+        <div className="w-5/12 ml-48 pt-20">
             <form>
                 <div className="form-group">
-                    <select name="driveType">
+                    <select name="driveType" className="form-control">
                         <option value="OneDrive">OneDrive</option>
                         <option value="SharePoint">SharePoint</option>
                         <option value="GoogleDrive">GoogleDrive</option>
                     </select>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" aria-describedby="emailHelp" />
+                    <input type="text" name="drive_id" placeholder="driveId" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <input type="text" placeholder="driveName" name="drive_name" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <input type="text" name="client_id" placeholder="client_id" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <input type="text" placeholder="client_secret" name="client_secret" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <button onClick={(e) => auth(e)} type="button" className="btn">提交</button>
                 </div>
             </form>
         </div>
