31736687af2b913909b9d310d382a06468d9ecc2
caoyufei <atcaoyufei@gmail.com>
2021-12-18 09:39:02 +0800

    0.46

--- src/components/Input.js
+++
@@ -1,8 +0,0 @@
-import {useState} from "react";
-
-export const UseInput = (props) => {
-    const [value, setValue] = useState(props.value || '');
-    return (
-        <input type="text" onChange={e => setValue(e.target.value)} name={props.name} placeholder={props.name} value={value} className="form-control"/>
-    )
-};
--- src/pages/AddDrive.js
+++ src/pages/AddDrive.js
@@ -1,13 +1,16 @@
 import type {FC} from "react";
 import {useState} from "react";
-import {UseInput} from "../components/Input";
 
 
 export const AddDrive: FC = () => {
-    const [client_id, setDriveId] = useState(null);
+    const [driveType, setDriveType] = useState(null);
+    const [drive_id, setDriveId] = useState(null);
+    const [drive_name, setDriveName] = useState(null);
+    const [client_id, setClientId] = useState(null);
+    const [client_secret, setClientSecret] = useState(null);
+
     const auth = (e) => {
         e.preventDefault()
-        console.log(client_id)
     }
     return (
         <div className="w-5/12 ml-48 pt-20">
@@ -23,14 +26,13 @@ export const AddDrive: FC = () => {
                     <input type="text" name="drive_id" onChange={e => setDriveId(e.target.value)} placeholder="driveId" className="form-control"/>
                 </div>
                 <div className="form-group">
-                    <input type="text" placeholder="driveName" name="drive_name"
-                           className="form-control"/>
+                    <input type="text" placeholder="driveName" onChange={e => setDriveName(e.target.value)} name="drive_name" className="form-control"/>
                 </div>
                 <div className="form-group">
-                    <UseInput name="client_id" />
+                    <input type="text" placeholder="client_id" onChange={e => setClientId(e.target.value)} name="client_id" className="form-control"/>
                 </div>
                 <div className="form-group">
-                    <UseInput name="client_secret" />
+                    <input type="text" placeholder="client_secret" onChange={e => setClientSecret(e.target.value)} name="client_secret" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <button onClick={(e) => auth(e)} type="button" className="btn">提交</button>
